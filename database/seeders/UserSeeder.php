57498ff8a9a097ac6da98a96f14866e820f4f48f
mrisqid <mrisqid@gmail.com>
2022-02-10 01:18:55 +0700

    add  UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      DB::table('users')->insert([
+        'username' => 'admin',
+        'email' => 'admin@gmail.com',
+        'password' => md5('admin123'),
+        'level' => 'admin',
+        'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+        'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+      ]);
+    }
+}
